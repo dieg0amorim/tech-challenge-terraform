19ed311a414648c2d200eff08d02f8009271383f
Diego Amorim <diego.devopslabs@outlook.com>
2024-01-28 20:03:24 -0300

    adicionando imagem ecs-pedidos

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,4 @@
-/*resource "aws_cloudwatch_log_group" "example" {
+resource "aws_cloudwatch_log_group" "example" {
   name              = "techchallenge3-log-group"
   retention_in_days = 7
 }
-*/
--- s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-unique-bucket-nametesteteste456"  # Substitua pelo nome desejado
-}
-
-resource "aws_s3_bucket" "example_bucket-2" {
-  bucket = "my-unique-bucket-nametesteteste123"  # Substitua pelo nome desejado
-}
