818719a2bd038b5accd3c18050887a45b8045c90
Diego Amorim <diego.devopslabs@outlook.com>
2024-01-28 22:34:09 -0300

    add aplicacao producao

--- app-producao.tf
+++ app-producao.tf
@@ -40,7 +40,7 @@ EOF
 }
 
 resource "aws_ecs_service" "my_service_producao" {
-  name            = "pedidos-service"
+  name            = "producao-service"
   cluster         = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.my_task-producao.arn
   launch_type     = "FARGATE"
