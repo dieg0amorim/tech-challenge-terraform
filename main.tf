c7835d622eb51da1eccef616cadfdbd4b8d74b1a
Diego Amorim <diego.devopslabs@outlook.com>
2024-01-28 21:32:28 -0300

    adicionando imagem ecs-financeiro

--- main.tf
+++ main.tf
@@ -15,3 +15,39 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_iam_policy" "ecs_execution_policy" {
+  name = "ecs_execution_policy"
+
+  description = "Policy for ECS task execution role"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:GetImage",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "secretsmanager:GetSecretValue"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy_attachment" {
+  policy_arn = aws_iam_policy.ecs_execution_policy.arn
+  role       = aws_iam_role.ecs_execution_role.name
+}
